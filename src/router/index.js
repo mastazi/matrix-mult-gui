465308e6c7ec6e4e81c74b6a55c04c1d97ebe061
Marco Stazi <marco.stazi@gmail.com>
2021-02-26 00:52:02 +1100

    Added help page

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    path: '/help',
+    name: 'Help',
+    // this generates a separate bundle (help.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "help" */ '../views/Help.vue')
     }
   }
 ]
