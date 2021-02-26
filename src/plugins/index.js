bd5246d4157d474bb67a05bb66677d1c5e6cb309
Marco Stazi <marco.stazi@gmail.com>
2021-02-26 11:39:03 +1100

    createded plugins, styles and images folders

---
+++ src/plugins/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import {
+  ModalPlugin,
+  BVModalPlugin,
+  InputGroupPlugin,
+  TablePlugin,
+  FormInputPlugin,
+  FormPlugin,
+  FormSelectPlugin,
+  AlertPlugin,
+  IconsPlugin,
+  NavPlugin,
+  PaginationPlugin,
+  SpinnerPlugin,
+  ListGroupPlugin,
+  CardPlugin,
+  AvatarPlugin,
+  CarouselPlugin,
+  ButtonPlugin,
+  TabsPlugin,
+  TooltipPlugin,
+  BadgePlugin,
+  CollapsePlugin,
+  OverlayPlugin,
+  FormTextareaPlugin,
+  FormCheckboxPlugin,
+} from "bootstrap-vue";
+
+Vue.use(ModalPlugin);
+Vue.use(BVModalPlugin);
+Vue.use(AlertPlugin);
+Vue.use(InputGroupPlugin);
+Vue.use(TablePlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormSelectPlugin);
+Vue.use(FormPlugin);
+Vue.use(IconsPlugin);
+Vue.use(NavPlugin);
+Vue.use(PaginationPlugin);
+Vue.use(SpinnerPlugin);
+Vue.use(ListGroupPlugin);
+Vue.use(CardPlugin);
+Vue.use(AvatarPlugin);
+Vue.use(CarouselPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(TabsPlugin);
+Vue.use(TooltipPlugin);
+Vue.use(BadgePlugin);
+Vue.use(CollapsePlugin);
+Vue.use(OverlayPlugin);
+Vue.use(FormTextareaPlugin);
+Vue.use(FormCheckboxPlugin);
