a8fc3a7c8a3d17494c8b6ac7323cc920183bd5f2
Marco Stazi <marco.stazi@gmail.com>
2021-02-27 10:40:00 +1100

    dimensions form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="home">
+    <h1>Multiply&hellip;</h1>
+    <b-form inline>
+      <b-form-group>
+        &hellip;a
+        <b-form-input v-model="matrix_x" type="number" required></b-form-input>
+        by
+        <b-form-input v-model="matrix_y" type="number" required></b-form-input>
+        matrix
+      </b-form-group>
+    </b-form>
+    <h1>by&hellip;</h1>
+    <b-form inline class="mb-4">
+      <b-form-group> </b-form-group>
+      <b-form-group>
+        &hellip;a
+        <b-form-input v-model="matrix_y" type="number" readonly></b-form-input>
+        by
+        <b-form-input v-model="matrix_x" type="number" readonly></b-form-input>
+        matrix
+      </b-form-group>
+    </b-form>
     <input-matrix></input-matrix>
     <result></result>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import InputMatrix from "../components/InputMatrix.vue";
-import Result from "../components/Result.vue";
+import InputMatrix from "@/components/InputMatrix.vue";
+import Result from "@/components/Result.vue";
 
 export default {
   name: "Home",
@@ -16,5 +36,17 @@ export default {
     InputMatrix,
     Result,
   },
+  data() {
+    return {
+      matrix_x: 3,
+      matrix_y: 2,
+    };
+  },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  max-width: 600px !important;
+  margin: 0 auto;
+}
+</style>
